687982f682460167fbcab84df5db1e1cb8be25e5
MOHTG <102815298+mazenorabi@users.noreply.github.com>
2024-10-17 11:18:55 +0200

    Update and rename my_git_dag.py to new_git_dag.py

--- new_git_dag.py
+++ new_git_dag.py
@@ -5,13 +5,13 @@ import pendulum
 import datetime
 
 with DAG(
-    dag_id="test_git_dag",
+    dag_id="new_git_dag",
     schedule=None,
     start_date=pendulum.datetime(2024, 10, 13, tz="UTC"),
     catchup=False,
     dagrun_timeout=datetime.timedelta(minutes=60),
     tags=["local", "test"],
-    params={"example_key": "example_value"},
+    #params={"example_key": "example_value"},
 ) as dag:
 
     run_this = BashOperator(
