08a8e0416891ea5b14c5a3eac62e3d14c37fac96
MOHTG <102815298+mazenorabi@users.noreply.github.com>
2024-10-17 11:34:14 +0200

    Update and rename new_git_dag.py to feature_git_dag.py

--- feature_git_dag.py
+++ feature_git_dag.py
@@ -5,18 +5,17 @@ import pendulum
 import datetime
 
 with DAG(
-    dag_id="new_git_dag",
+    dag_id="feature_git_dag",
     schedule=None,
     start_date=pendulum.datetime(2024, 10, 13, tz="UTC"),
     catchup=False,
     dagrun_timeout=datetime.timedelta(minutes=60),
     tags=["local", "test"],
-    #params={"example_key": "example_value"},
 ) as dag:
 
     run_this = BashOperator(
         task_id="print_var",
-        bash_command="echo Testing - {{ var.value.get('TEST_VAR') }}",
+        bash_command="echo Feature - {{ var.value.get('TEST_VAR') }}",
     )
     
 
