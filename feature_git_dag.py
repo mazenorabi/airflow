6ca1719be9cc74d0c2c62cf8edf794e80f3bdea8
MOHTG <102815298+mazenorabi@users.noreply.github.com>
2024-10-18 11:20:51 +0200

    Update feature_git_dag.py

--- feature_git_dag.py
+++ feature_git_dag.py
@@ -1,11 +1,15 @@
-from airflow.models.dag import DAG
+#from airflow.models.dag import DAG
+from .py_utils.dags_model import HtgDAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.empty import EmptyOperator
 import pendulum
 import datetime
 
-with DAG(
+with HtgDAG(
     dag_id="feature_git_dag",
+    severity=3,
+    slack_alerts_channel_name="",
+    model_name="",
     schedule=None,
     start_date=pendulum.datetime(2024, 10, 13, tz="UTC"),
     catchup=False,
